7f4a4f9f14d4c05e2f564b24ee50b56e5b3857ee
python <1554461798@qq.com>
2018-02-01 14:50:29 +0800

    Initial commit

---
+++ views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/1 14:45
+# @Author  : 金钰
+# @Email   : XXXXXXX@163.com
+# @File    : views.py
+# @Software: PyCharm Community Edition
+
+print 'asdfvfdgds'
